359a84ab075c5421739396b36f24bb4e5619d059
Parakh Chaudhary <69101765+parakhc4@users.noreply.github.com>
2026-04-25 19:33:14 +0530

    Init Springboot project with dependencies

---
+++ src/main/java/com/upstrail/EmpMGMT/EmpMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.upstrail.EmpMGMT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpMgmtApplication.class, args);
+	}
+
+}
